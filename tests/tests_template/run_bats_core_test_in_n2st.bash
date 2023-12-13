fc84427d5e25915fadee43295d1fe94af355223d
RedLeaderOne <luc.coupal.1@ulaval.ca>
2023-12-13 04:32:06 -0500

    fix: run_bats_core_test_in_n2st.bash argument passing to inner function

--- tests/tests_template/run_bats_core_test_in_n2st.bash
+++ tests/tests_template/run_bats_core_test_in_n2st.bash
@@ -18,6 +18,8 @@
 #   Read N2ST_PATH    Default to "./utilities/norlab-shell-script-tools"
 #
 # =================================================================================================
+OPTIONS="$@"
+
 function n2st::run_n2st_testsing_tools(){
   local TMP_CWD
   TMP_CWD=$(pwd)
@@ -28,7 +30,7 @@ function n2st::run_n2st_testsing_tools(){
 
   # ....Execute N2ST run_bats_tests_in_docker.bash.................................................
   cd "$NBS_PATH"
-  bash "${N2ST_PATH}/tests/bats_testing_tools/run_bats_tests_in_docker.bash" "$@"
+  bash "${N2ST_PATH}/tests/bats_testing_tools/run_bats_tests_in_docker.bash" "$OPTIONS"
 
   # ....Teardown...................................................................................
   cd "$TMP_CWD"
